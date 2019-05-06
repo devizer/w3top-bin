efe6cf94dc321d05968464904b8fe265e4ecf873
devizer <devizer@github.io>
2019-05-06 21:06:00 +0300

    added build.sh
    Former-commit-id: 4e4246dd4b6da7ab616a297d3e12bc32370344c0

---
+++ build/build-w3-dashboard.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# wget -q -nv --no-check-certificate -O - https://raw.githubusercontent.com/devizer/KernelManagementLab/master/build-w3-dashboard.sh | bash -s reinstall_service  
+set -e
+set -u
+if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; elif [[ $(uname -m) == aarch64 ]]; then rid=linux-arm64; elif [[ $(uname -m) == x86_64 ]]; then rid=linux-x64; fi; if [[ $(uname -s) == Darwin ]]; then rid=osx-x64; fi;
+echo "The current OS architecture: $rid"
+
+function header() { LightGreen='\033[1;32m';Yellow='\033[1;33m';RED='\033[0;31m'; NC='\033[0m'; printf "${LightGreen}$1${NC} ${Yellow}$2${NC}\n"; }
+counter=0;
+function say() { counter=$((counter+1)); header "STEP $counter" "$1"; }
+
+
+work=$HOME/transient-builds
+if [[ -d "/transient-builds" ]]; then work=/transient-builds; fi
+if [[ -d "/ssd" ]]; then work=/ssd/transient-builds; fi
+work=$work/publish/KernelManagementLab;
+# work=/mnt/ftp-client/KernelManagementLab;
+mkdir -p "$(dirname $work)"
+cd $(dirname $work);
+rm -rf $work;
+git clone https://github.com/devizer/KernelManagementLab;
+cd KernelManagementLab/Universe.W3Top
+dir=$(pwd)
+
+pushd ../build >/dev/null
+./inject-git-info.ps1
+popd >/dev/null
+
+cd ClientApp; time (yarn install); cd ..
+for r in linux-x64 linux-arm linux-arm64; do
+  say "Building $r"
+  time dotnet publish -c Release /p:DefineConstants="DUMPS" -o bin/$r/w3top --self-contained -r $r
+  pushd bin/$r
+  say "Compressing $r as gzip"
+  time sudo bash -c "tar cf - w3top | pv | gzip -9 > ../w3top-$r.tar.gz"
+  popd
+done
+
