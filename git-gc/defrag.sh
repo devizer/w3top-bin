a9707a1cb7bfce7d8ce72824c02f30e958d33f8d
devizer <devizer@github.io>
2019-05-06 20:18:50 +0300

    Empty Initial
    Former-commit-id: 3cd39bb419fb71ba8ccf899c90bdd9907ae9e64d

---
+++ git-gc/defrag.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set repo=w3-top
+work=/transient-builds/defrag
+pushd "$(dirname $0)" >/dev/null; ScriptDir="$(pwd)"; popd >/dev/null
+mkdir -p $work
+pushd $work
+rm -rf $repo.git
+git clone --mirror git@github.com:devizer/$repo
+java -jar $ScriptDir/bfg-1.13.0.jar --strip-blobs-bigger-than 1M $repo.git
+
+cd $repo.git
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
+git push
+
+popd
+
