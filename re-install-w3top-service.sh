918881cabe2d48eb1ce24823231df319649a8443
devizer <devizer@users.github.io>
2021-05-24 20:42:15 +0300

    fix dockerhub throttling
    Former-commit-id: c08a37614c873ea6e933a72f6159cb1d8311905d

---
+++ re-install-w3top-service.sh
@@ -0,0 +1,8 @@
+﻿echo '#!/usr/bin/env bash
+export HTTP_PORT=5050
+export RESPONSE_COMPRESSION=True
+export INSTALL_DIR=/opt/w3top
+script=https://raw.githubusercontent.com/devizer/w3top-bin/master/install-w3top-service.sh
+(wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
+' | sudo tee /usr/local/bin/re-install-w3top-service
+sudo chmod +x /usr/local/bin/re-install-w3top-service
