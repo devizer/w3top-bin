61f6e45b8262cf71f3cdf33e56b68333210ef9c8
devizer <devizer@github.users>
2022-01-04 05:14:07 +0200

    fix timout for tests
    Former-commit-id: bf9721b5f0d37d3c4810505080bb2dd16d799e1e

--- tests/w3top-tests.sh
+++ tests/w3top-tests.sh
@@ -11,12 +11,12 @@ function install_dependencies() {
 }
 
 function wait_for_http() {
-  u="$1"; t=90; 
+  u="$1"; t=30; 
   printf "Waiting for [$u] during $t seconds ..."
   while [ $t -ge 0 ]; do 
     t=$((t-1)); 
     e1=249;
-    if [[ "$(command -v curl)" != "" ]]; then curl -m 1 -skf "$u" >/dev/null; e1=$?; fi
+    if [[ "$(command -v curl)" != "" ]]; then curl --connect-timeout 3 -skf "$u" >/dev/null; e1=$?; fi
     if [[ "$e1" -ne 0 ]]; then
       if [[ "$(command -v wget)" != "" ]]; then wget -q -nv -T 1 "$u" >/dev/null; e1=$?; fi
     fi
