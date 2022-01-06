3dba045e5581ca1a5c57a092052ddb8a266b9cfe
devizer <devizer@github.users>
2022-01-06 16:02:14 +0200

    fix syntax for tests
    Former-commit-id: 57d4fcfc4cde3e103f3e714c70d27feb06bed4d1

--- tests/w3top-tests.sh
+++ tests/w3top-tests.sh
@@ -48,7 +48,7 @@ function install_w3top() {
   sslver=$(get_openssl_system_version)
   if [[ "$sslver" == 3* ]]; then 
     export OPENSSL_HOME=/opt/openssl
-    Say "Building openssl 1.1 to [$OPENSSL_HOME]
+    Say "Building openssl 1.1 to [$OPENSSL_HOME]"
     install_opensll_111
     export APP_LD_LIBRARY_PATH=$OPENSSL_HOME/lib
   fi
