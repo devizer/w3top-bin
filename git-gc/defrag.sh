e4d70370bc227bfaea16cdf3db99c11299cb88e6
devizer <devizer@users.noreply.github.com>
2022-05-29 11:22:08 +0300

    Update defrag.sh
    Former-commit-id: c6cbdaf0b6fc42f46442d7fd89e835dd188db0d9

--- git-gc/defrag.sh
+++ git-gc/defrag.sh
@@ -9,9 +9,11 @@ rm -rf $repo.git
 git clone --mirror git@github.com:devizer/$repo
 java -jar $ScriptDir/bfg-1.13.0.jar --strip-blobs-bigger-than 1M $repo.git
 
-
 cd $repo.git
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
+ver=$(cat ../public/version.txt)
+echo "VERSION $ver"
+git commit --allow-empty -m "Update $ver"
 git push
 
 popd
