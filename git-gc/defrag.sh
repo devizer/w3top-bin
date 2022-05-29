97322e71e91d265763fab481dd9af64fcd3d4dfb
devizer <devizer@users.noreply.github.com>
2022-05-29 23:27:47 +0300

    Update defrag.sh
    Former-commit-id: 273e84801b2a46eb1aa717fdb1993b567d84dd3b

--- git-gc/defrag.sh
+++ git-gc/defrag.sh
@@ -4,6 +4,7 @@ set -eu;
 pushd "$(dirname $0)" >/dev/null; ScriptDir="$(pwd)"; popd >/dev/null
 
 repo=w3top-bin
+giturl="git@github.com:devizer"
 work=/transient-builds/defrag
 Say "Degrag/strip [$repo] repo using [$work] working folder"
 echo "Current folder: [$(pwd)]"
@@ -11,7 +12,7 @@ echo "Current folder: [$(pwd)]"
 mkdir -p $work
 pushd $work
 rm -rf $repo.git
-git clone --mirror git@github.com:devizer/$repo
+git clone --mirror $giturl/$repo
 java -jar $ScriptDir/bfg-1.13.0.jar --strip-blobs-bigger-than 1M $repo.git
 
 cd $repo.git
@@ -24,10 +25,8 @@ if [[ -z "${TRIGGER_COMMIT_MESSAGE:-}" ]]; then exit 0; fi
 
 Say "Trigger [$repo] pipelines using [$work-again] folder"
 echo "Current folder: [$(pwd)]"
-git clone $w3topBinRepo $work-again
+git clone $giturl/$repo $work-again
 pushd $work-again
-ver=$(cat ./public/version.txt)
-echo "VERSION [$ver]"
-git commit --allow-empty -m "Update $ver"
+git commit --allow-empty -m "${TRIGGER_COMMIT_MESSAGE}"
 git push
 popd
