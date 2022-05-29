46b8efc031e207d0351d62ee3caf831bcb6864fd
devizer <devizer@users.noreply.github.com>
2022-05-29 23:05:04 +0300

    Update defrag.sh
    Former-commit-id: dc6ec43b9a50a8f2774436a39282651beb24aafc

--- git-gc/defrag.sh
+++ git-gc/defrag.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+set -eu;
+pushd "$(dirname $0)" >/dev/null; ScriptDir="$(pwd)"; popd >/dev/null
+
 repo=w3top-bin
 work=/transient-builds/defrag
-pushd "$(dirname $0)" >/dev/null; ScriptDir="$(pwd)"; popd >/dev/null
+Say "Degrag/strip [$repo] repo using [$work] working folder"
+echo "Current folder: [$(pwd)]"
+
 mkdir -p $work
 pushd $work
 rm -rf $repo.git
@@ -11,6 +16,18 @@ java -jar $ScriptDir/bfg-1.13.0.jar --strip-blobs-bigger-than 1M $repo.git
 
 cd $repo.git
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
+Say "Push stripped [$repo] repo"
 git push
+popd
 
+if [[ -z "${TRIGGER_COMMIT_MESSAGE:-}" ]]; then exit 0; fi
+
+Say "Trigger [$repo] pipelines using [$work-again] folder"
+echo "Current folder: [$(pwd)]"
+git clone $w3topBinRepo $work-again
+pushd $work-again
+ver=$(cat ./public/version.txt)
+echo "VERSION [$ver]"
+git commit --allow-empty -m "Update $ver"
+git push
 popd
