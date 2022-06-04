8d9d56f8ea4c36fb929c776ac048da10cd1533e1
devizer <devizer@github.io>
2022-06-04 14:01:12 +0300

    Smarty install ***NO_CI***
    Former-commit-id: e60d9093dc816b7e4a827eeba2848aecd15d2eb1

--- install-w3top-service.sh
+++ install-w3top-service.sh
@@ -2,6 +2,91 @@
 set -e
 set -u
 
+NC='\033[0m' Color_Green='\033[1;32m' Color_Red='\033[1;31m' Color_Yellow='\033[1;33m'; 
+function say() { 
+   local var="Color_${1:-}"
+   local color="${!var}"
+   shift 
+   printf "${color:-}$*${NC}\n";
+}
+
+function find_decompressor() {
+  COMPRESSOR_EXT=""
+  COMPRESSOR_EXTRACT=""
+  if [[ -n "${GCC_FORCE_GZIP_PRIORITY:-}" ]]; then
+    if [[ "$(command -v gzip)" != "" ]]; then
+      COMPRESSOR_EXT=gz
+      COMPRESSOR_EXTRACT="gzip -f -d"
+    elif [[ "$(command -v xz)" != "" ]]; then
+      COMPRESSOR_EXT=xz
+      COMPRESSOR_EXTRACT="xz -f -d"
+    fi
+  else
+    if [[ "$(command -v xz)" != "" ]]; then
+      COMPRESSOR_EXT=xz
+      COMPRESSOR_EXTRACT="xz -f -d"
+    elif [[ "$(command -v gzip)" != "" ]]; then
+      COMPRESSOR_EXT=gz
+      COMPRESSOR_EXTRACT="gzip -f -d"
+    fi
+  fi
+}
+
+function download_file_fialover() {
+  local file="$1"
+  shift
+  for url in "$@"; do
+    # DEBUG: echo -e "\nTRY: [$url] for [$file]"
+    local err=0;
+    download_file "$url" "$file" || err=$?
+    # DEBUG: say Green "Download status for [$url] is [$err]"
+    if [ "$err" -eq 0 ]; then return; fi
+  done
+  return 55;
+}
+
+try_count=0
+function download_file() {
+  local url="$1"
+  local file="$2";
+  local progress1="" progress2="" progress3="" 
+  if [[ "$DOWNLOAD_SHOW_PROGRESS" != "True" ]] || [[ ! -t 1 ]]; then
+    progress1="-q -nv"       # wget
+    progress2="-s"           # curl
+    progress3="--quiet=true" # aria2c
+  fi
+  rm -f "$file" 2>/dev/null || rm -f "$file" 2>/dev/null || rm -f "$file"
+  local try1=""
+  if [[ -z "${SKIP_ARIA:-}" ]] && [[ "$(command -v aria2c)" != "" ]]; then
+    [[ -n "${try1:-}" ]] && try1="$try1 || "
+    try1="aria2c $progress3 --allow-overwrite=true --check-certificate=false -s 9 -x 9 -k 1M -j 9 -d '$(dirname "$file")' -o '$(basename "$file")' '$url'"
+  fi
+  if [[ -z "${SKIP_CURL:-}" ]] && [[ "$(command -v curl)" != "" ]]; then
+    [[ -n "${try1:-}" ]] && try1="$try1 || "
+    try1="${try1:-} curl $progress2 -f -kSL -o '$file' '$url'"
+  fi
+  if [[ -z "${SKIP_WGET:-}" ]] && [[ "$(command -v wget)" != "" ]]; then
+    [[ -n "${try1:-}" ]] && try1="$try1 || "
+    try1="${try1:-} wget $progress1 --no-check-certificate -O '$file' '$url'"
+  fi
+  if [[ "${try1:-}" == "" ]]; then
+    echo "error: niether curl, wget or aria2c is available"
+    exit 42;
+  fi
+  local i=0;
+  for i in 1 2 3; do
+    try_count=$((try_count+1))
+    if [[ "${try_count}" -gt 1 ]]; then
+      say Red "Try #${try_count} to download $url"
+    fi
+     local err=""
+     say Green "$try1"
+     eval $try1 || err=1
+     if [[ -z "$err" ]]; then return; fi
+  done
+  return 55;
+}
+
 machine="$(uname -m || true)"; machine="${machine:-unknown}"
 rid=unknown
 if [[ "$machine" == armv7* ]]; then
@@ -29,7 +114,10 @@ elif [ -e /etc/redhat-release ]; then
 fi
 echo "The OS architecture: $rid"
 
-file=w3top-$rid.tar.gz
+
+find_decompressor
+
+file="w3top-$rid.tar.${COMPRESSOR_EXT}"
 url_version=https://raw.githubusercontent.com/devizer/w3top-bin/master/public/version.txt
 version=$(wget -q -nv --no-check-certificate -O - $url_version 2>/dev/null || curl -ksfL $url_version 2>/dev/null || echo "unknown")
 url_primary=https://github.com/devizer/KernelManagementLab/releases/download/v$version/$file
@@ -39,7 +127,16 @@ sha256=$(wget -q -nv --no-check-certificate -O - $url_sha256 2>/dev/null || curl
 
 url_tertiary=https://raw.githubusercontent.com/devizer/w3top-bin/master/public/$file
 
+url_4=https://sourceforge.net/projects/w3top/files/$version/w3top-rhel.6-x64.tar.xz.sha256/download
+url_5="https://master.dl.sourceforge.net/project/w3top/$version/w3top-rhel.6-x64.tar.xz.sha256?viasf=1"
 
+[[ -n "${SKIP_URL_1:-}" ]] && url_primary=""
+[[ -n "${SKIP_URL_2:-}" ]] && url_tertiary=""
+[[ -n "${SKIP_URL_3:-}" ]] && url_4=""
+[[ -n "${SKIP_URL_4:-}" ]] && url_5=""
+
+
+TMPDIR="${TMPDIR:-/tmp}"
 HTTP_PORT="${HTTP_PORT:-5050}"
 RESPONSE_COMPRESSION="${RESPONSE_COMPRESSION:-True}"
 INSTALL_DIR="${INSTALL_DIR:-/opt/w3top}"
@@ -53,27 +150,30 @@ echo "W3Top installation parameters:
     RESPONSE_COMPRESSION: $RESPONSE_COMPRESSION
 Internal installer variables:
     version per metadata (optional): $version
-    primary download url: $url_primary
-    secondary download url: $url_secondary
-    tertiary download url: $url_tertiary
+    download url #1: $url_primary
+    download url #2: $url_tertiary
+    download url #3: $url_4
+    download url #4: $url_5
     sha256 hash: $sha256
     temp download file: $copy
 "
 
 mkdir -p "$(dirname $copy)"
 ok="false"
-for url in "$url_primary" "$url_secondary" "$url_tertiary"; do
-  wget --no-check-certificate -O "$copy" "$url"  || curl -kfSL -o "$copy" "$url" || continue;
-  
-  # fileSize=$(stat --printf="%s" "$copy")
-  fileSize=$(stat -c"%s" "$copy" 2>/dev/null || stat --printf="%s" "$copy")
-  echo "Downloaded size of \"$file\" is $fileSize bytes"
-  if [[ ! $fileSize > 40000000 ]]; then continue; fi
-
-  if [[ "$url" != "$url_tertiary" ]]; then 
-    echo "Checking integrity of $(basename $url_primary) ..."
-    if echo "$sha256 $copy" | sha256sum -c - ; then ok=true; break; fi
-  else ok=true; fi;
+for url in "$url_primary" "$url_tertiary" "$url_4" "$url_5"; do
+  if [[ -z "$url" ]]; then continue; fi
+  export DOWNLOAD_SHOW_PROGRESS=True
+  err=""; download_file "${url}" "$copy" || err=1
+  if [[ -n "$err" ]]; then continue; fi
+  actual_hash="$(sha256sum "$copy" | awk '{print $1}')"
+  if [[ "${actual_hash:-}" != "$sha256" ]]; then
+    echo "SHA256 mismatch: ${actual_hash:-}"
+    continue;
+  else
+    echo "SHA256 hash is correct. Extracting $copy to [$INSTALL_DIR]"
+    ok=true
+    break;
+  fi
 done
 
 if [[ $ok != true ]]; then echo Error downloading $(basename "$url_primary"); exit 1; fi
@@ -82,10 +182,31 @@ sudo mkdir -p "$INSTALL_DIR"
 sudo rm -rf "$INSTALL_DIR/*"
 pushd "$INSTALL_DIR" >/dev/null
 if [[ ! -z "$(command -v pv)" ]]; then
-  pv "$copy" | sudo tar xzf -
+  # pv "$copy" | sudo tar xzf -
+  pv "$copy" | eval $COMPRESSOR_EXTRACT | sudo tar xf - 2>&1 | { grep -v "implausibly old time stamp" || true; } | { grep -v "in the future" || true; }
 else
-  sudo tar xzf "$copy"
+  # sudo tar xzf "$copy"
+  cat "$copy" | eval $COMPRESSOR_EXTRACT | sudo tar xf - 2>&1 | { grep -v "implausibly old time stamp" || true; } | { grep -v "in the future" || true; }
 fi
 sudo rm -f "$copy"
 bash install-systemd-service.sh
 popd >/dev/null
+
+echo 'Test
+
+function try_install() {
+  export HTTP_HOST=0.0.0.0 HTTP_PORT=5050
+  export RESPONSE_COMPRESSION=True
+  export INSTALL_DIR=/opt/w3top
+  script=https://raw.githubusercontent.com/devizer/w3top-bin/master/install-w3top-service.sh
+  (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
+}
+sudo systemctl disable w3top.service 2>/dev/null || true
+for GCC_FORCE_GZIP_PRIORITY in True ""; do
+  export GCC_FORCE_GZIP_PRIORITY;
+  export SKIP_ARIA=True SKIP_CURL=True SKIP_WGET="";   try_install
+  export SKIP_ARIA=True SKIP_CURL=""   SKIP_WGET=True; try_install
+  export SKIP_ARIA=""   SKIP_CURL=True SKIP_WGET=TRUE; try_install
+done
+Say "COMPLETE SUCCESS"
+' >/dev/null
---
+++ tests/Manual-Test-Install.sh
@@ -0,0 +1,16 @@
+set -eu;
+function try_install() {
+  export HTTP_HOST=0.0.0.0 HTTP_PORT=5050
+  export RESPONSE_COMPRESSION=True
+  export INSTALL_DIR=/opt/w3top
+  script=https://raw.githubusercontent.com/devizer/w3top-bin/master/install-w3top-service.sh
+  (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
+}
+sudo systemctl disable w3top.service 2>/dev/null || true
+for GCC_FORCE_GZIP_PRIORITY in True ""; do
+  export GCC_FORCE_GZIP_PRIORITY;
+  export SKIP_ARIA=True SKIP_CURL=True SKIP_WGET="";   try_install
+  export SKIP_ARIA=True SKIP_CURL=""   SKIP_WGET=True; try_install
+  export SKIP_ARIA=""   SKIP_CURL=True SKIP_WGET=TRUE; try_install
+done
+Say "COMPLETE SUCCESS"
